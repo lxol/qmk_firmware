d87a1a682663909ed43c9914f8f3d52ddc085341
Alex Olkhovskiy <alex@olkhovskiy.com>
2017-03-10 06:44:51 +0000

    wip

--- keyboards/planck/keymaps/lxol-exp/keymap.c
+++ keyboards/planck/keymaps/lxol-exp/keymap.c
@@ -42,28 +42,35 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  { KC_ESC ,  KC_Q ,    KC_W ,  KC_E ,    KC_R ,   KC_T ,    KC_Y ,   KC_U ,  KC_I ,    KC_O ,    KC_P ,    KC_BSPC } , 
-  { KC_TAB ,  KC_A ,    KC_S ,  KC_D ,    KC_F ,   KC_G ,    KC_H ,   KC_J ,  KC_K ,    KC_L ,    KC_SCLN , KC_ENT } ,  
-  { KC_LSFT , KC_Z ,    KC_X ,  KC_C ,    KC_V ,   KC_B ,    KC_N ,   KC_M ,  KC_COMM , KC_DOT ,  KC_SLSH , KC_RSFT } , 
-  { KC_F24 ,  KC_F23 ,  MOUSE , KC_LALT , LOWER ,  KC_LGUI , KC_SPC , RAISE , KC_RCTL , KC_RALT , SUB_LR ,  _______ }
+  { KC_ESC ,  KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,   KC_U ,  KC_I ,    KC_O ,    KC_P ,    KC_BSPC } , 
+  { KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,   KC_J ,  KC_K ,    KC_L ,    KC_SCLN , KC_ENT } ,  
+  { KC_BSLS , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,  KC_COMM , KC_DOT ,  KC_SLSH , KC_QUOT } , 
+  { KC_F24 ,  MOUSE ,   KC_LALT , LOWER ,   KC_LSFT , KC_LGUI , KC_SPC , RAISE , KC_RCTL , KC_MINS , KC_EQL , XXXXXXX }
  } ,          
 
 [_LOWER] = { 
-  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , _______ , _______ , KC_BSPC } , 
-  { KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
- } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_PGDN , KC_PGUP , _______ ,  _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ }
+ } ,                   
+
+/* [_RAISE] = { */
+/*   { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    KC_BSPC } ,  */
+/*   { KC_DEL ,  KC_LBRC , KC_RBRC , KC_ENT ,  KC_ENT ,  _______ , _______ , KC_MINS , KC_EQL ,  _______ , KC_BSLS , _______ } ,  */
+/*   { _______ , _______ , _______ , _______ , _______ , KC_BSPC , _______ , KC_QUOT , _______ , _______ , _______ , _______ } ,  */
+/*   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } */
+/*  } ,           */
 
 [_RAISE] = {
-  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    KC_BSPC } , 
-  { _______ , KC_LBRC , KC_LPRN , KC_LCBR , KC_MINS , KC_EQL ,  _______ , KC_QUOT , KC_PIPE , _______ , _______ , _______ } , 
-  { _______ , KC_RBRC , KC_RPRN , KC_RCBR , KC_UNDS , KC_PLUS , _______ , KC_DQT ,  KC_BSLS , _______ , _______ , _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
- } ,          
+  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    _______ } , 
+  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , _______ } , 
+  { KC_PIPE , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR , _______ , _______ , _______ , _______ , _______ , _______ , _______ } , 
+  { _______ , KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
+ } , 
 
 [_MOUSE] = { 
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_WH_D , KC_WH_U , _______ , _______ , KC_BSPC } , 
+  { _______ ,  _______ , _______ , _______ , _______ , _______ , _______ , KC_WH_D , KC_WH_U , _______ , _______ , KC_BSPC } , 
   { _______ , _______ , KC_ACL0 , KC_ACL1 , KC_ACL2 , _______ , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , _______ , _______ } , 
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_BTN1 , KC_BTN3 , KC_BTN2 , _______ , _______ } , 
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
@@ -88,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 const uint16_t PROGMEM fn_actions[] = {
   [0] = ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),
-  [1] = ACTION_MODS_TAP_KEY(MOD_RCTL, KC_SCLN)
+  [1] = ACTION_MODS_TAP_KEY(MOD_LGUI, KC_BSPC)
 };
 
 #ifdef AUDIO_ENABLE
