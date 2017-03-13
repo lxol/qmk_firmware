3e36972a17884c539700ddabca5ee374c4c4086f
Alex Olkhovskiy <alex@olkhovskiy.com>
2017-03-13 06:52:27 +0000

    wip

--- keyboards/planck/keymaps/lxol-exp/keymap.c
+++ keyboards/planck/keymaps/lxol-exp/keymap.c
@@ -54,24 +54,24 @@ const uint16_t PROGMEM fn_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = {
-  { KC_ESC ,  KC_Q ,    KC_W ,  KC_E ,  KC_R ,    KC_T ,     KC_Y ,     KC_U ,       KC_I ,     KC_O ,    KC_P ,    KC_MINS } , 
-  { KC_TAB ,  KC_A ,    KC_S ,  KC_D ,  KC_F ,    KC_G ,     KC_H ,     KC_J ,       KC_K ,     KC_L ,    KC_SCLN , KC_QUOT } , 
-  { KC_LALT , KC_Z ,    KC_X ,  KC_C ,  KC_V ,    KC_B ,     KC_N ,     KC_M ,       KC_COMM ,  KC_DOT ,  KC_SLSH , KC_EQL } ,  
-  { KC_F24 ,  XXXXXXX , MOUSE , LOWER , SFT_ENT , LGUI_ENT , KC_SPACE , RAISE_BSPC , CTL_BSLS , XXXXXXX , XXXXXXX , XXXXXXX }
- } , 
+  { KC_ESC ,  KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,     KC_U ,  KC_I ,    KC_O ,    KC_P ,    KC_MINS } , 
+  { KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,     KC_J ,  KC_K ,    KC_L ,    KC_SCLN , KC_QUOT } , 
+  { KC_LALT , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,     KC_M ,  KC_COMM , KC_DOT ,  KC_SLSH , KC_EQL } ,  
+  { KC_F24 ,  XXXXXXX , MOUSE ,   LOWER ,   KC_LSFT , KC_LGUI , KC_SPACE , RAISE , KC_LCTL , XXXXXXX , KC_BSPC , KC_ENT }
+ } ,          
 
 [_LOWER] = { 
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_PGDN , KC_PGUP , _______ ,  _______ , _______ } , 
   { _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
   { KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,   KC_F10 ,   KC_F11 ,  KC_F12 } ,  
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_MNXT , KC_VOLD ,  KC_VOLU , KC_MPLY }
+  { _______ , _______ , _______ , _______ , _______ , _______ , KC_BSPC , _______ , _______ , KC_VOLD ,  KC_VOLU , KC_MPLY }
  } ,          
 
 [_RAISE] = { 
   { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    _______ } , 
   { _______ , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR , _______ , _______ , KC_LPRN , KC_RPRN , _______ , _______ , _______ } , 
   { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , _______ , _______ , _______ } , 
-  { _______ , KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
+  { _______ , KC_DEL ,  _______ , _______ , KC_ENT ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ }
  } ,          
 
 [_MOUSE] = { 
