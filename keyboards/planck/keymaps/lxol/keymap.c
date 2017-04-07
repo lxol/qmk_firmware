d1e73f7b34b514d30f6ab39ad33db40bab8e3c75
Alex Olkhovskiy <alex@olkhovskiy.com>
2017-04-07 08:39:09 +0100

    add FUN layer

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -8,12 +8,14 @@ enum planck_layers {
   _QWERTY,
   _LOWER,
   _RAISE,
+  _FUN,
   _MOUSE
 };
 
 enum planck_keycodes {
   LOWER = SAFE_RANGE,
   RAISE,
+  FUN,
   MOUSE
 };
 
@@ -26,21 +28,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_ESC ,  KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,   KC_U ,  KC_I ,    KC_O ,    KC_P ,    KC_MINS } , 
   { KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,   KC_J ,  KC_K ,    KC_L ,    KC_SCLN , KC_QUOT } , 
   { KC_PLUS , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,  KC_COMM , KC_DOT ,  KC_SLSH , KC_EQL } ,  
-  { KC_F24 ,  MOUSE ,   LOWER ,   LOWER ,   KC_LSFT , KC_LGUI , KC_SPC , RAISE , KC_LCTL , _______ , KC_BSPC , KC_ENT }
+  { KC_F24 ,  MOUSE ,   FUN ,     LOWER ,   KC_LSFT , KC_LGUI , KC_SPC , RAISE , KC_LCTL , _______ , KC_BSPC , KC_ENT }
  } ,          
 
 [_LOWER] = { 
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_PGDN , KC_PGUP , _______ ,  _______ , _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } , 
-  { KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,   KC_F10 ,   KC_F11 ,  KC_F12 } ,  
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_PGDN , KC_PGUP , KC_LPRN ,  KC_RPRN , _______ } , 
+  { _______ , _______ , _______ , KC_LPRN , KC_RPRN , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_BSLS , KC_PIPE , _______ ,  _______ , _______ } , 
   { KC_CAPS , BL_DEC ,  BL_INC ,  _______ , _______ , _______ , _______ , _______ , _______ , KC_VOLD ,  KC_VOLU , KC_MPLY }
- } ,          
+ } ,                               
 
 [_RAISE ] = { 
   { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    _______ } , 
   { _______ , KC_LBRC , KC_RBRC , KC_LCBR , KC_RCBR , KC_BSPC , KC_EQL ,  KC_ENT ,  KC_EQL ,  KC_BSLS , KC_PIPE , _______ } , 
-  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN , _______ } , 
+  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , _______ , _______ , _______ } , 
   { _______ , KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
+ } ,          
+
+[_FUN] = { 
+  { KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,   KC_F10 ,  KC_F11 ,  KC_F12 } ,  
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_VOLD , KC_VOLU , KC_MPLY }
  } ,          
 
 [_MOUSE] = { 
@@ -76,6 +85,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         layer_on(_MOUSE);
       } else {
         layer_off(_MOUSE);
+      }
+      return false;
+      break;
+    case FUN:
+      if (record->event.pressed) {
+        layer_on(_FUN);
+      } else {
+        layer_off(_FUN);
       }
       return false;
       break;
