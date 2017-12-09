3887ac66ebf7d9670503b52746785380bbcfc61c
Alex Olkhovskiy <alex@olkhovskiy.com>
2017-12-09 20:00:23 +0000

    wip

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -39,41 +39,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  } ,
 
 [_LEFT] = {
-  { DYN_REC_STOP , DYN_REC_START1 , DYN_REC_START2 , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGDN ,       KC_PGUP , XXXXXXX ,  _______ , _______ } ,
-  { XXXXXXX ,      BACKLIT ,        XXXXXXX ,        XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN ,       KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } ,
-  { _______ ,      XXXXXXX ,        XXXXXXX ,        XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , LCTL(KC_MINS) , _______ , _______ ,  _______ , _______ } ,
-  { _______ ,      BL_DEC ,         BL_INC ,         _______ , _______ , _______ , _______ , _______ ,       _______ , _______ ,  KC_DEL ,  _______ }
- } ,
+  { DYN_REC_STOP , DYN_REC_START1 ,  DYN_REC_START2 ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGDN , KC_PGUP , XXXXXXX ,  _______ , _______ } , 
+  { XXXXXXX ,      BACKLIT ,         XXXXXXX ,         XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } , 
+  { _______ ,      XXXXXXX ,         XXXXXXX ,         XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ , _______ , _______ ,  _______ , _______ } , 
+  { _______ ,      BL_DEC ,          BL_INC ,          _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  KC_DEL ,  _______ }
+ } ,               
 
-[_RIGHT] = {
-  { _______ , DYN_MACRO_PLAY1 , DYN_MACRO_PLAY2 , KC_LBRC , KC_RBRC , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
-  { XXXXXXX , XXXXXXX ,         XXXXXXX ,         KC_LCBR , KC_RCBR , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
-  { _______ , XXXXXXX ,         XXXXXXX ,         KC_LPRN , KC_RPRN , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ } ,
+[_RIGHT] = { 
+  { _______ , DYN_MACRO_PLAY1 , DYN_MACRO_PLAY2 , KC_LBRC , KC_RBRC , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } , 
+  { XXXXXXX , XXXXXXX ,         XXXXXXX ,         KC_LCBR , KC_RCBR , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } , 
+  { _______ , XXXXXXX ,         XXXXXXX ,         KC_LPRN , KC_RPRN , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , _______ } , 
   { _______ , _______ ,         _______ ,         _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } /*  */
- } ,
+ } ,          
 
-[_RAISE ] = {
-  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    _______ } ,
-  { _______ , KC_LBRC , KC_RBRC , KC_LPRN , KC_RPRN , KC_BSPC , KC_EQL ,  KC_ENT ,  KC_ESC , KC_MINS , _______ , _______ } ,
-  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , _______ , _______ , _______ } ,
+[_RAISE ] = { 
+  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    _______ } , 
+  { _______ , KC_LBRC , KC_RBRC , KC_LPRN , KC_RPRN , KC_BSPC , KC_EQL ,  KC_ENT ,  KC_ESC ,  KC_MINS , _______ , _______ } , 
+  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , _______ , _______ , _______ } , 
   { KC_FN0 ,  KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ }
- } ,
+ } ,          
 
-[_FUN] = {
-  { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,   KC_F10 ,  KC_F11 } ,
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ } ,
-  { AG_NORM , AG_SWAP , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_VOLU , _______ } ,
-  { KC_F8 ,   KC_F9 ,   _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_VOLD , KC_MPLY }
- } ,
+[_FUN] = { 
+  { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,  
+  { XXXXXXX , BACKLIT , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } , 
+  { AG_NORM , AG_SWAP , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  KC_VOLU , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  KC_VOLD , KC_MPLY }
+ } , 
 
 [_LEFTRIGHT] = {
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ ,   _______ ,   _______ ,   _______ ,   _______ , _______ } ,
-  { _______ , _______ , _______ , _______ , _______ , _______ , MEH(KC_H) , MEH(KC_J) , MEH(KC_K) , MEH(KC_L) , _______ , _______ } ,
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ ,   _______ ,   _______ ,   _______ ,   _______ , _______ } ,
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ ,   _______ ,   _______ ,   _______ ,   _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , MEH(KC_H) , MEH(KC_J) , MEH(KC_K) , MEH(KC_L) , _______ , _______ } , 
+  { _______ , _______ , _______ , _______ , _______ , _______ , _______ ,   _______ ,   _______ ,   _______ ,   _______ , _______ } , 
   { KC_CAPS , _______ , _______ , _______ , _______ , _______ , _______ ,   _______ ,   _______ ,   _______ ,   _______ , _______ }
- },
+ } ,          
 
-[_MOUSE] = {
+[_MOUSE] = { 
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_WH_D , KC_WH_U , _______ , _______ , _______ } ,
   { _______ , KC_ACL0 , KC_ACL1 , KC_ACL2 , _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , _______ , _______ } ,
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_BTN1 , KC_BTN3 , KC_BTN2 , _______ , _______ } ,
