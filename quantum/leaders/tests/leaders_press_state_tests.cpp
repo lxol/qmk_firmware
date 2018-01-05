0b3665b378d103cb3ae7bb6cb1d0f388274c6406
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-01-05 20:51:57 +0000

    rename pressstate to press_state

---
+++ quantum/leaders/tests/leaders_press_state_tests.cpp
@@ -0,0 +1,34 @@
+/* Copyright 2017 Alex Olkhovskiy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "gtest/gtest.h"
+#include <cstdarg>
+#include "quantum_keycodes.h"
+extern "C" {
+#include "leaders/press_state.h"
+}
+class Pressstate : public testing::Test {
+
+public:
+  Pressstate() {
+    init_press_state();
+  }
+  virtual ~Pressstate() {
+  }
+};
+
+TEST_F(Pressstate, init_press_state_works ) {
+  ASSERT_EQ(press_state_get(), 0UL );
+}
