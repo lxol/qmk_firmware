07709c022c695362eb0266c50310d732af1e8174
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-01-15 06:56:47 +0000

    add rev4MS

---
+++ keyboards/planck/rev4MS/config.h
@@ -0,0 +1,13 @@
+#ifndef REV4_CONFIG_H
+#define REV4_CONFIG_H
+
+#include "config_common.h"
+
+#define DEVICE_VER 0x0004
+
+#undef VENDOR_ID
+#define VENDOR_ID       0x04D9
+#undef PRODUCT_ID
+#define PRODUCT_ID      0x2013
+
+#endif
