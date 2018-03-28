0bdc948ece1e6395b8c48e4e706f8fd642bfdda7
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-03-28 14:06:38 +0100

    update planck/lxol keymap.c

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_DOUBLERAISE] = {
   { _______ , KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,     KC_0 ,    _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
+  { _______ , KC_TILD , KC_EXLM , KC_MINS , KC_PLUS , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
   { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,  
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ }
  } ,          
@@ -201,10 +201,13 @@ bool keyseq_press_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code16(KC_RGUI);
       } else {
         register_code16(kc);
-        unregister_code16(kc);
       }
       return false ;
     } else {
+      uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
+      if (kc != KC_NO && record->event.key.row != 0) {
+        unregister_code16(kc);
+      } 
       return false;
     }
   case SEQ_IE:
