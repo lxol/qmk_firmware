cfd07900f1ad99489a6577dc9055df0159502455
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-04-02 10:34:12 +0100

    Update lxol keymap.
    Use leaders for modifier combination.

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -11,6 +11,7 @@ enum planck_layers {
   _DOUBLERAISE,
   _FUN,
   _SYM,
+  _MODIFIER,
   _MOUSE
 };
 
@@ -23,12 +24,12 @@ enum planck_keycodes {
   FUN,
   MOUSE,
   BACKLIT,
-  LD_SYMBOLS,
   LD_RAISE,
 
   SEQ_SYMBOLS,
   SEQ_RAISE,
   SEQ_DOUBLERAISE,
+  SEQ_MODIFIERS,
   SEQ_LEFT,
   SEQ_RIGHT,
   SEQ_UP,
@@ -36,6 +37,11 @@ enum planck_keycodes {
   SEQ_IE,
   SEQ_ID,
   SEQ_IC,
+  SEQ_EXPAND,
+  SEQ_CONTRACT,
+  SEQ_CTLALT,
+  SEQ_CTLGUI,
+  SEQ_GUIALT,
   DYNAMIC_MACRO_RANGE,
 };
 
@@ -50,37 +56,43 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = {
   { KC_ESC ,  KC_Q ,    KC_W , KC_E ,    KC_R ,    KC_T ,    KC_Y ,   KC_U ,     KC_I ,    KC_O ,   KC_P ,    KC_MINS } ,
   { KC_TAB ,  KC_A ,    KC_S , KC_D ,    KC_F ,    KC_G ,    KC_H ,   KC_J ,     KC_K ,    KC_L ,   KC_SCLN , KC_QUOT } ,
-  { XXXXXXX , KC_Z ,    KC_X , KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,     KC_COMM , KC_DOT , KC_SLSH , KC_PLUS } ,
-  { XXXXXXX , KC_RGUI , FUN ,  KC_LGUI , KC_LSFT , KC_LALT , KC_SPC , LD_RAISE , KC_LCTL , KC_DEL ,  KC_RGUI , KC_ENT }
+  { KC_PIPE , KC_Z ,    KC_X , KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,     KC_COMM , KC_DOT , KC_SLSH , KC_PLUS } ,
+  { KC_BSLS , KC_RGUI , FUN ,  KC_LGUI , KC_LSFT , KC_LALT , KC_SPC , LD_RAISE , KC_LCTL , KC_DEL , KC_RGUI , KC_ENT }
  } ,
 [_RAISE] = {
-  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,       KC_9 ,    KC_0 ,    _______ } ,
-  { _______ , KC_LBRC , KC_RBRC , _______ , _______ , KC_BSPC , KC_EQL ,  KC_ENT ,  LD_SYMBOLS , _______ , _______ , _______ } ,
-  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR ,    _______ , _______ , _______ } ,
-  { CALTDEL , KC_DEL ,  _______ , _______ , _______ , _______ , _______ , _______ , _______ ,    _______ , _______ , _______ }
+  { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    XXXXXXX } ,
+  { XXXXXXX , KC_LBRC , KC_RBRC , KC_LPRN , KC_RPRN , KC_BSPC , KC_EQL ,  KC_ENT ,  XXXXXXX , KC_LCBR , KC_RCBR , XXXXXXX } ,
+  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , XXXXXXX , XXXXXXX , XXXXXXX } ,
+  { CALTDEL , KC_DEL ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX }
  } ,
 
 [_DOUBLERAISE] = {
-  { _______ , KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,     KC_0 ,    _______ } , 
-  { _______ , _______ , _______ , _______ , _______ , _______ , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , _______ , _______ } , 
-  { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,  
-  { _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ }
- } ,          
+  { XXXXXXX , KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,     KC_0 ,    XXXXXXX } ,
+  { XXXXXXX , KC_TILD , KC_EXLM , KC_MINS , KC_PLUS , KC_DEL ,  KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , XXXXXXX , XXXXXXX } ,
+  { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,  XXXXXXX , XXXXXXX }
+ } ,
 
-[_FUN] = { 
-  { KC_F12 ,       KC_F1 ,           KC_F2 ,           KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,  
-  { DYN_REC_STOP , DYN_REC_START1 ,  DYN_REC_START2 ,  BACKLIT , BL_DEC ,  BL_INC ,  KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } , 
-  { KC_CAPS ,      DYN_MACRO_PLAY1 , DYN_MACRO_PLAY2 , KC_VOLU , KC_VOLD , KC_MPLY , _______ , KC_PGDN , KC_PGUP , _______ ,  _______ , _______ } , 
+[_FUN] = {
+  { KC_F12 ,       KC_F1 ,           KC_F2 ,           KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,
+  { DYN_REC_STOP , DYN_REC_START1 ,  DYN_REC_START2 ,  BACKLIT , BL_DEC ,  BL_INC ,  KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_BSLS , KC_PIPE } ,
+  { KC_CAPS ,      DYN_MACRO_PLAY1 , DYN_MACRO_PLAY2 , KC_VOLU , KC_VOLD , KC_MPLY , _______ , KC_PGDN , KC_PGUP , _______ ,  _______ , _______ } ,
   { _______ ,      _______ ,         _______ ,         _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ }
- } ,               
+ } ,
 
-[_SYM] = { 
-  { XXXXXXX , KC_GRV , KC_QUOT , KC_LCBR , KC_RCBR , KC_BSLS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } , 
-  { XXXXXXX , KC_TILD , KC_DQUO , KC_LPRN , KC_RPRN , KC_BSPC , XXXXXXX , KC_ENT , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } , 
-  { XXXXXXX , XXXXXXX , XXXXXXX , KC_LBRC , KC_RBRC , KC_PIPE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } , 
+[_SYM] = {
+  { XXXXXXX , KC_GRV ,  KC_QUOT , KC_LCBR , KC_RCBR , KC_BSLS , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , KC_TILD , KC_DQUO , KC_LPRN , KC_RPRN , KC_BSPC , XXXXXXX , KC_ENT ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX , KC_LBRC , KC_RBRC , KC_PIPE , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
   { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX }
- } , 
+ } ,
 
+[_MODIFIER] = {
+  { XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , XXXXXXX , SEQ_GUIALT , SEQ_CTLGUI , SEQ_CTLALT , XXXXXXX , XXXXXXX , SEQ_CTLALT , SEQ_CTLGUI , SEQ_GUIALT , XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX , XXXXXXX ,    XXXXXXX ,    XXXXXXX ,    XXXXXXX , XXXXXXX }
+ } ,
 [_MOUSE] = {
   { _______ , _______ , _______ , _______ , _______ , _______ , _______ , KC_WH_D , KC_WH_U , _______ , _______ , _______ } ,
   { _______ , KC_ACL0 , KC_ACL1 , KC_ACL2 , _______ , _______ , KC_MS_L , KC_MS_D , KC_MS_U , KC_MS_R , _______ , _______ } ,
@@ -140,19 +152,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #ifdef LEADERS_ENABLE
 
 uint16_t* user_definitions[]  = {
-  (uint16_t[]){5, LD_SYMBOLS, KC_I, KC_E, SEQ_IE },
-  (uint16_t[]){5, LD_SYMBOLS, KC_I, KC_D, SEQ_ID },
-  (uint16_t[]){5, LD_SYMBOLS, KC_I, KC_C, SEQ_IC },
-  (uint16_t[]){4, LD_SYMBOLS, KC_TRNS, SEQ_SYMBOLS },
   (uint16_t[]){6, LD_RAISE, KC_K, KC_I, KC_E, SEQ_IE },
   (uint16_t[]){6, LD_RAISE, KC_K, KC_I, KC_D, SEQ_ID },
   (uint16_t[]){6, LD_RAISE, KC_K, KC_I, KC_C, SEQ_IC },
   (uint16_t[]){5, LD_RAISE, KC_K, KC_TRNS, SEQ_SYMBOLS },
-  /* (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_H, SEQ_LEFT }, */
-  /* (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_J, SEQ_DOWN }, */
-  /* (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_K, SEQ_UP }, */
-  /* (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_L, SEQ_RIGHT }, */
+  (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_K, SEQ_EXPAND },
+  (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_J, SEQ_CONTRACT },
   (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_DOUBLERAISE },
+  (uint16_t[]){5, LD_RAISE, KC_L, KC_TRNS, SEQ_MODIFIERS },
   (uint16_t[]){4, LD_RAISE, KC_TRNS, SEQ_RAISE },
   (uint16_t[]){1}
 };
@@ -201,10 +208,13 @@ bool keyseq_press_user(uint16_t keycode, keyrecord_t *record) {
         unregister_code16(KC_RGUI);
       } else {
         register_code16(kc);
-        unregister_code16(kc);
       }
       return false ;
     } else {
+      uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
+      if (kc != KC_NO && record->event.key.row != 0) {
+        unregister_code16(kc);
+      }
       return false;
     }
   case SEQ_IE:
@@ -234,38 +244,66 @@ bool keyseq_press_user(uint16_t keycode, keyrecord_t *record) {
     } else {
       return false;
     }
-  /* case SEQ_LEFT: */
-  /*   if (record->event.pressed) { */
-  /*     register_code16(KC_LEFT); */
-  /*     return false; */
-  /*   } else { */
-  /*     unregister_code16(KC_LEFT); */
-  /*     return false; */
-  /*   } */
-  /* case SEQ_RIGHT: */
-  /*   if (record->event.pressed) { */
-  /*     register_code16(KC_RIGHT); */
-  /*     return false; */
-  /*   } else { */
-  /*     unregister_code16(KC_RIGHT); */
-  /*     return false; */
-  /*   } */
-  /* case SEQ_UP: */
-  /*   if (record->event.pressed) { */
-  /*     register_code16(KC_UP); */
-  /*     return false; */
-  /*   } else { */
-  /*     unregister_code16(KC_UP); */
-  /*     return false; */
-  /*   } */
-  /* case SEQ_DOWN: */
-  /*   if (record->event.pressed) { */
-  /*     register_code16(KC_DOWN); */
-  /*     return false; */
-  /*   } else { */
-  /*     unregister_code16(KC_DOWN); */
-  /*     return false; */
-  /*   } */
+    // emacs er/expand-region C-s-u C-s-d
+  case SEQ_EXPAND:
+    if (record->event.pressed) {
+      register_code16(KC_LGUI);
+      register_code16(KC_LCTL);
+      register_code16(KC_U);
+      return false;
+    } else {
+      unregister_code16(KC_U);
+      unregister_code16(KC_LCTL);
+      unregister_code16(KC_LGUI);
+      return false;
+    }
+  case SEQ_CONTRACT:
+    if (record->event.pressed) {
+      register_code16(KC_LGUI);
+      register_code16(KC_LCTL);
+      register_code16(KC_D);
+      return false;
+    } else {
+      unregister_code16(KC_D);
+      unregister_code16(KC_LCTL);
+      unregister_code16(KC_LGUI);
+      return false;
+    }
+  case SEQ_MODIFIERS: {
+    uint16_t kc = keymap_key_to_keycode(_MODIFIER, record->event.key);
+    if (record->event.pressed) {
+      switch (kc) {
+      case SEQ_CTLALT:
+        register_code16(KC_LCTL);
+        register_code16(KC_LALT);
+        return false;
+      case SEQ_CTLGUI:
+        register_code16(KC_LCTL);
+        register_code16(KC_LGUI);
+        return false;
+      case SEQ_GUIALT:
+        register_code16(KC_LGUI);
+        register_code16(KC_LALT);
+        return false;
+      }
+      return false;
+    } else {
+      switch (kc) {
+      case SEQ_CTLALT:
+        unregister_code16(KC_LALT);
+        unregister_code16(KC_LCTL);
+        return false;
+      case SEQ_CTLGUI:
+        unregister_code16(KC_LGUI);
+        unregister_code16(KC_LCTL);
+        return false;
+      case SEQ_GUIALT:
+        unregister_code16(KC_LALT);
+        unregister_code16(KC_LGUI);
+        return false;
+      }
+      return false;
+    }}
   }
   return false;
 }
