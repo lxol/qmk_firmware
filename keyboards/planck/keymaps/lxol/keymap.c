c59d786c6c28067d025744aed6b5a05143df553d
Alex Olkhovskiy <alex@olkhovskiy.com>
2018-11-16 09:04:38 +0000

    wip

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -69,29 +69,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_ESC ,   KC_Q ,    KC_W ,    KC_E ,     KC_R ,    KC_T ,    KC_Y ,   KC_U ,     KC_I ,    KC_O ,   KC_P ,    KC_MINS } ,
   { KC_TAB ,   KC_A ,    KC_S ,    KC_D ,     KC_F ,    KC_G ,    KC_H ,   KC_J ,     KC_K ,    KC_L ,   KC_SCLN , KC_QUOT } ,
   { LD_LOWER , KC_Z ,    KC_X ,    KC_C ,     KC_V ,    KC_B ,    KC_N ,   KC_M ,     KC_COMM , KC_DOT , KC_SLSH , KC_PLUS } ,
-  { FUN ,      KC_LGUI , LD_PAIRS,  KC_RGUI , KC_LSFT , KC_LALT , KC_SPC , LD_RAISE , KC_LCTL , KC_DEL , KC_RGUI , KC_ENT }
+  { FUN ,      KC_LGUI , KC_RGUI,  LD_LOWER , KC_LSFT , KC_LALT , KC_SPC , LD_RAISE , KC_LCTL , KC_DEL , KC_RGUI , KC_ENT }
  } ,
 
 [_RAISE] = {
   { KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    XXXXXXX } ,
-  { KC_PIPE , KC_LBRC , KC_RBRC , KC_LPRN,  KC_RPRN,  KC_BSPC , KC_EQL ,  KC_ENT ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
+  { XXXXXXX , KC_GRV , KC_ESC ,   KC_ESC,   KC_ESC,   KC_BSPC , KC_EQL ,  KC_ENT ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } ,
   { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , XXXXXXX , XXXXXXX , XXXXXXX } ,
   { CALTDEL , KC_DEL ,  XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX }
  } ,
 
 [_DOUBLERAISE] = {
   { XXXXXXX , KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    XXXXXXX } ,
-  { KC_TAB ,  KC_TILD , KC_LBRC , KC_LCBR , KC_LPRN , KC_BSPC , KC_LEFT , KC_RPRN , KC_RCBR , KC_RBRC , KC_PIPE , KC_BSLS } ,
+  { XXXXXXX , KC_ESC ,  KC_ESC ,  KC_LCBR , KC_LPRN , KC_BSPC , KC_LEFT , KC_RPRN , KC_RCBR , KC_RBRC , KC_PIPE , KC_BSLS } ,
   { KC_F12 ,  KC_F1 ,   KC_F2 ,   KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,   KC_F10 ,  KC_F11 } ,
   { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX }
  } ,
 
-[_DOUBLELOWER] = {
-  { XXXXXXX , KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,   KC_U ,    KC_I ,    KC_O ,    KC_P ,    KC_MINS } ,
-  { KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,   KC_J ,    KC_K ,    KC_L ,    KC_SCLN , KC_QUOT } ,
-  { XXXXXXX , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,    KC_COMM , KC_DOT ,  KC_SLSH , KC_PLUS } ,
-  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_SPC , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX }
+[_LOWER] = {
+  { KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN , KC_RPRN ,  KC_UNDS } ,
+  { XXXXXXX , KC_TILD , KC_ESC , KC_ESC , KC_ESC , KC_BSPC , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_COLON , KC_DQT } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LCBR , KC_LBRC , KC_RBRC , KC_RCBR ,  KC_BSLS , XXXXXXX } ,
+  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_SPC ,  XXXXXXX , XXXXXXX , XXXXXXX ,  XXXXXXX , XXXXXXX }
  } ,
+
+/* [_DOUBLELOWER] = { */
+/*   { XXXXXXX , KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,   KC_U ,    KC_I ,    KC_O ,    KC_P ,    KC_MINS } , */
+/*   { KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,   KC_J ,    KC_K ,    KC_L ,    KC_SCLN , KC_QUOT } , */
+/*   { XXXXXXX , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,   KC_M ,    KC_COMM , KC_DOT ,  KC_SLSH , KC_PLUS } , */
+/*   { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_SPC , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX } */
+/*  } , */
 
 [_FUN] = {
   { KC_F12 ,       KC_F1 ,           KC_F2 ,           KC_F3 ,   KC_F4 ,   KC_F5 ,   KC_F6 ,   KC_F7 ,   KC_F8 ,   KC_F9 ,    KC_F10 ,  KC_F11 } ,
@@ -100,12 +107,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { _______ ,      _______ ,         _______ ,         _______ , _______ , _______ , _______ , _______ , _______ , _______ ,  _______ , _______ }
  } ,
 
-[_LOWER] = {
-  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LPRN , KC_LPRN , KC_RPRN , KC_RPRN ,  XXXXXXX , KC_PIPE } ,
-  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_QUOT , KC_DQT } ,
-  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LCBR , KC_LBRC , KC_RBRC , KC_RCBR ,  KC_BSLS , XXXXXXX } ,
-  { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_SPC ,  XXXXXXX , XXXXXXX , XXXXXXX ,  XXXXXXX , XXXXXXX }
- } ,
 
 [_PAIRS] = {
   { XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_I ,    KC_LPRN , KC_RPRN , KC_PIPE } ,
@@ -197,9 +198,10 @@ uint16_t* user_definitions[]  = {
   /* (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_K, SEQ_EXPAND }, */
   /* (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_J, SEQ_CONTRACT }, */
   (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_DOUBLERAISE },
+  /* (uint16_t[]){5, LD_RAISE, LD_LOWER, KC_TRNS, SEQ_LOWER }, */
   /* (uint16_t[]){5, LD_RAISE, KC_L, KC_TRNS, SEQ_MODIFIERS }, */
   (uint16_t[]){4, LD_RAISE, KC_TRNS, SEQ_RAISE },
-  (uint16_t[]){5, LD_LOWER, LD_LOWER, KC_TRNS, SEQ_DOUBLELOWER },
+  /* (uint16_t[]){5, LD_LOWER, LD_LOWER, KC_TRNS, SEQ_DOUBLELOWER }, */
   (uint16_t[]){5, LD_LOWER, LD_RAISE, KC_TRNS, SEQ_RAISE },
   (uint16_t[]){4, LD_LOWER, KC_TRNS, SEQ_LOWER },
   (uint16_t[]){5, LD_PAIRS, KC_I, KC_O, SEQ_PRNPAIR },
