30c90551b1363321b48358604d379c178d65c7c3
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-04-03 08:44:37 +0100

    update lxol atreus keymap

--- keyboards/atreus/keymaps/lxol/keymap.c
+++ keyboards/atreus/keymaps/lxol/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, LD_LOWER1, LD_LOWER1,  KC_LSFT, KC_LALT,  KC_, KC_ESC, LD_SPC,  LD_RAISE, KC_LCTL, XXXXXXX, KC_ENT
+    KC_ESC, LD_LOWER1, LD_LOWER1,  KC_LSFT, KC_LALT,  KC_ESC,  KC_ENT, KC_SPC,  LD_RAISE, KC_LCTL, XXXXXXX, KC_ENT
   ),
 
   /*
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_1, KC_2,   KC_3,   KC_4, KC_5,                              KC_6, KC_7,    KC_8,   KC_9, KC_0 ,
-    KC_TAB, KC_GRV, KC_LBRC, KC_RBRC, KC_BSPC,                    KC_ESC, KC_ENT,    XXXXXXX,   KC_QUOT, XXXXXXX,
+    KC_TAB, KC_ESC, KC_LBRC, KC_RBRC, KC_BSPC,                    KC_ESC, KC_ENT,    XXXXXXX,   KC_QUOT, XXXXXXX,
     KC_EXLM, KC_AT,   KC_HASH,   KC_DLR, KC_PERC,                  KC_CIRC, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX ,
     /* KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX,                   XXXXXXX,  XXXXXXX,    XXXXXXX,   KC_EQL, KC_BSLS, */
     XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
