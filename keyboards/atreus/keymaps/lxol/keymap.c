8dc04e7adc110b860a28d0b38c678a3911e9654c
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-04-03 09:08:08 +0100

    atreus

--- keyboards/atreus/keymaps/lxol/keymap.c
+++ keyboards/atreus/keymaps/lxol/keymap.c
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, LD_LOWER1, LD_LOWER1,  KC_LSFT, KC_LALT,  KC_ESC,  KC_ENT, KC_SPC,  LD_RAISE, KC_LCTL, XXXXXXX, KC_ENT
+    KC_ESC, LD_LOWER1, LD_LOWER1,  KC_LSFT, KC_LALT,  KC_ESC,  KC_ENT, KC_SPC,  LD_RAISE, KC_LCTL, KC_RGUI, KC_ENT
   ),
 
   /*
@@ -101,12 +101,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX,  XXXXXXX,    XXXXXXX,   XXXXXXX, XXXXXXX, */
   /*   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX), */
 
+  /* [_DOUBLERAISE] = LAYOUT( /\* [> RAISE <] *\/ */
+  /*   XXXXXXX, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR , */
+  /*   KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS , */
+  /*   KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS , */
+  /*   TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  XXXXXXX, KC_DOT, KC_0, KC_EQL  ), */
+
   [_DOUBLERAISE] = LAYOUT( /* [> RAISE <] */
-    XXXXXXX, KC_AT,   KC_UP,   KC_LCBR, KC_RCBR,                   KC_PGUP, KC_7,    KC_8,   KC_9, KC_ASTR ,
+    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_HASH, KC_LEFT, KC_DOWN, KC_RGHT, KC_DLR,                    KC_PGDN, KC_4,    KC_5,   KC_6, KC_PLUS ,
     KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_AMPR,                   KC_GRV,  KC_1,    KC_2,   KC_3, KC_BSLS ,
-    TG(_LW), KC_INS,  KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  XXXXXXX, KC_DOT, KC_0, KC_EQL  ),
-
+    XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX  ),
 
   [_LOWER] = LAYOUT( /* [> RAISE <] */
     KC_EXLM, KC_AT,   KC_HASH,   KC_DLR, KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR,   KC_PLUS, KC_MINS ,
@@ -184,7 +189,7 @@ uint16_t* user_definitions[]  = {
   /* (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_K, SEQ_EXPAND }, */
   /* (uint16_t[]){6, LD_RAISE, LD_RAISE, KC_LSFT, KC_J, SEQ_CONTRACT }, */
   /* (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_DOUBLERAISE }, */
-  (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_RAISE2 },
+  (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_DOUBLERAISE },
   /* (uint16_t[]){5, LD_RAISE, KC_L, KC_TRNS, SEQ_MODIFIERS }, */
   (uint16_t[]){4, LD_RAISE, KC_TRNS, SEQ_RAISE },
   (uint16_t[]){4, LD_LOWER, KC_TRNS, SEQ_LOWER },
@@ -294,20 +299,17 @@ bool keyseq_press_user(uint16_t keycode, keyrecord_t *record) {
   case SEQ_DOUBLERAISE:
     if (record->event.pressed) {
       uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
-      if (kc != KC_NO && record->event.key.row == 0) {
-        register_code16(KC_RGUI);
-        register_code16(kc);
-        unregister_code16(kc);
-        unregister_code16(KC_RGUI);
-      } else {
-        register_code16(kc);
-      }
+      register_code16(KC_RGUI);
+      register_code16(KC_SLSH);
+      unregister_code16(KC_SLSH);
+      unregister_code16(KC_RGUI);
+      register_code16(kc);
       return false ;
     } else {
       uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
-      if (kc != KC_NO && record->event.key.row != 0) {
-        unregister_code16(kc);
-      }
+      unregister_code16(kc);
+      register_code16(KC_ESC);
+      unregister_code16(KC_ESC);
       return false;
     }
   case SEQ_PAIRS:
