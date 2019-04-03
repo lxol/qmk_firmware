353c33338ff9efb3e667a776b5e3c4a16db38348
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-04-03 12:06:55 +0100

    wip

--- keyboards/atreus/keymaps/lxol/keymap.c
+++ keyboards/atreus/keymaps/lxol/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT( /* [> RAISE <] */
     KC_1, KC_2,   KC_3,   KC_4, KC_5,                              KC_6, KC_7,    KC_8,   KC_9, KC_0 ,
     KC_TAB, KC_ESC, KC_LBRC, KC_RBRC, KC_BSPC,                    KC_ESC, KC_ENT,    XXXXXXX,   KC_QUOT, XXXXXXX,
-    KC_EXLM, KC_AT,   KC_HASH,   KC_DLR, KC_PERC,                  KC_CIRC, XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX ,
+    KC_EXLM, KC_AT,   KC_HASH,   KC_DLR, KC_PERC,                  KC_CIRC, KC_AMPR, KC_ASTR,   XXXXXXX, XXXXXXX ,
     /* KC_LBRC, KC_RBRC, KC_LCBR, KC_RCBR, XXXXXXX,                   XXXXXXX,  XXXXXXX,    XXXXXXX,   KC_EQL, KC_BSLS, */
     XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SPC,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX),
 
