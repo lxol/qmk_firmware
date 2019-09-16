7820265711028c5fab7707f9d0f6e75f57098f2a
Alex Olkhovskiy <lxol@users.noreply.github.com>
2019-09-16 09:47:53 +0100

    wip

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -62,10 +62,10 @@ enum planck_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT_planck_grid(
-   KC_ESC ,  KC_Q ,    KC_W ,    KC_E ,     KC_R ,    KC_T ,     KC_Y ,   KC_U ,     KC_I ,    KC_O ,    KC_P ,    KC_MINS  ,
-   KC_TAB ,  KC_A ,    KC_S ,    KC_D ,     KC_F ,    KC_G ,     KC_H ,   KC_J ,     KC_K ,    KC_L ,    KC_SCLN , KC_QUOT  ,
-   KC_TILD , KC_Z ,    KC_X ,    KC_C ,     KC_V ,    KC_B ,     KC_N ,   KC_M ,     KC_COMM , KC_DOT ,  KC_SLSH , KC_PLUS  ,
-   XXXXXXX , XXXXXXX , KC_LALT , LD_LOWER , KC_LSFT , LD_LOWER2, KC_SPC , LD_RAISE , KC_LCTL , XXXXXXX , XXXXXXX , XXXXXXX 
+   KC_ESC ,  KC_Q ,    KC_W ,     KC_E ,     KC_R ,    KC_T ,     KC_Y ,   KC_U ,     KC_I ,    KC_O ,    KC_P ,    KC_MINS  ,
+   KC_TAB ,  KC_A ,    KC_S ,     KC_D ,     KC_F ,    KC_G ,     KC_H ,   KC_J ,     KC_K ,    KC_L ,    KC_SCLN , KC_QUOT  ,
+   KC_TILD , KC_Z ,    KC_X ,     KC_C ,     KC_V ,    KC_B ,     KC_N ,   KC_M ,     KC_COMM , KC_DOT ,  KC_SLSH , KC_PLUS  ,
+   XXXXXXX , XXXXXXX , KC_RGUI,   LD_LOWER2 , KC_LSFT , KC_LALT,   KC_SPC , LD_RAISE , KC_LCTL , XXXXXXX , XXXXXXX , XXXXXXX 
                                ),
 
 [_RAISE] = LAYOUT_planck_grid (
@@ -90,10 +90,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                ), 
 
 [_LOWER2] = LAYOUT_planck_grid (
-   KC_TILD , KC_EXLM , KC_AT ,   KC_HASH , KC_DLR ,  KC_PERC , KC_CIRC , KC_AMPR , KC_ASTR , KC_LPRN ,  KC_RPRN , XXXXXXX  ,
-   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN , KC_UP ,   KC_RIGHT , KC_LBRC , KC_RBRC  ,
-   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LCBR ,  KC_RCBR , XXXXXXX  ,
-   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LALT , KC_SPC ,  XXXXXXX , XXXXXXX , XXXXXXX ,  XXXXXXX , XXXXXXX
+   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_END  , XXXXXXX , KC_HOME , XXXXXXX ,  XXXXXXX , KC_LPRN ,  KC_RPRN , KC_BSPC  ,
+   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LEFT , KC_DOWN ,  KC_UP ,   KC_RIGHT , KC_LBRC , KC_RBRC  ,
+   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_PGDOWN, KC_PGUP , KC_LCBR ,  KC_RCBR , KC_END  ,
+   XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_LALT , KC_SPC ,  XXXXXXX ,  XXXXXXX , XXXXXXX ,  XXXXXXX , XXXXXXX
                                ), 
 
 [_FUN] = LAYOUT_planck_grid(
