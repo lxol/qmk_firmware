a3bbc8492fdbb0ee2ff93a455a9aeb054146b2fe
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-11-09 09:27:45 +0000

    wip

--- keyboards/planck/keymaps/lxol/keymap.c
+++ keyboards/planck/keymaps/lxol/keymap.c
@@ -85,9 +85,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                ),
 
 [_DOUBLERAISE] = LAYOUT_planck_grid (
-   XXXXXXX , KC_Q ,    KC_W ,    KC_E ,    KC_R ,    KC_T ,    KC_Y ,    KC_U ,    KC_I ,    KC_O ,    KC_P ,    KC_MINS  ,
-   KC_TAB ,  KC_A ,    KC_S ,    KC_D ,    KC_F ,    KC_G ,    KC_H ,    KC_J ,    KC_K ,    KC_L ,    KC_SCLN , KC_QUOT  ,
-   XXXXXXX , KC_Z ,    KC_X ,    KC_C ,    KC_V ,    KC_B ,    KC_N ,    KC_M ,    KC_COMM , KC_DOT ,  KC_SLSH , KC_PLUS  ,
+   KC_GRV ,  KC_1 ,    KC_2 ,    KC_3 ,    KC_4 ,    KC_5 ,    KC_6 ,    KC_7 ,    KC_8 ,    KC_9 ,    KC_0 ,    KC_MINS  ,
+   XXXXXXX , KC_LCBR , KC_RCBR , KC_LPRN , KC_RPRN , KC_BSPC,  KC_LEFT , KC_DOWN ,  KC_UP ,   KC_RIGHT , KC_LBRC , KC_RBRC  ,
+   XXXXXXX , XXXXXXX , XXXXXXX , KC_LBRC , KC_RBRC , KC_PIPE,  XXXXXXX , KC_PGDOWN, KC_PGUP , KC_LCBR ,  KC_RCBR , KC_END  ,
    XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX
                                      ), 
 
@@ -140,7 +140,7 @@ uint16_t* user_definitions[]  = {
   (uint16_t[]){5, LD_RAISE, KC_K,     KC_TRNS, SEQ_SYMBOLS },
   (uint16_t[]){5, LD_RAISE, KC_A,     KC_TRNS, SEQ_FUN },
   /* (uint16_t[]){5, LD_RAISE, LD_RAISE, LD_RAISE, SEQ_TRIPLERAISE }, */
-  (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_LOWER2 },
+  (uint16_t[]){5, LD_RAISE, LD_RAISE, KC_TRNS, SEQ_DOUBLERAISE },
   (uint16_t[]){4, LD_RAISE, KC_TRNS,  SEQ_RAISE },
   (uint16_t[]){5, LD_LOWER, LD_RAISE, KC_TRNS, SEQ_RAISE },
   (uint16_t[]){4, LD_LOWER, KC_TRNS,  SEQ_LOWER },
@@ -285,17 +285,17 @@ bool keyseq_press_user(uint16_t keycode, keyrecord_t *record) {
   case SEQ_DOUBLERAISE:
     if (record->event.pressed) {
       uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
-      register_code16(KC_RGUI);
-      register_code16(KC_SLSH);
-      unregister_code16(KC_SLSH);
-      unregister_code16(KC_RGUI);
+      if (kc==KC_1||kc==KC_2||kc==KC_3||kc==KC_4||kc==KC_5||kc==KC_6||kc==KC_7||kc==KC_8||kc==KC_9||kc==KC_0) {
+        register_code16(KC_RGUI);
+      }
       register_code16(kc);
       return false ;
     } else {
       uint16_t kc = keymap_key_to_keycode(_DOUBLERAISE, record->event.key);
       unregister_code16(kc);
-      register_code16(KC_ESC);
-      unregister_code16(KC_ESC);
+      if (kc==KC_1||kc==KC_2||kc==KC_3||kc==KC_4||kc==KC_5||kc==KC_6||kc==KC_7||kc==KC_8||kc==KC_9||kc==KC_0) {
+        unregister_code16(KC_RGUI);
+      }
       return false;
     }
   case SEQ_FUN:
